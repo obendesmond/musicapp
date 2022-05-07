0e5ff00a2a338ef23e9ed74c1ae188ef199f26da
obendesmond <obendesmond2@gmail.com>
2022-05-07 02:14:42 +0100

    music app completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
-import "./App.css";
-import Header from "./sections/Header";
+import './App.css';
+import Download from './components/Download';
+import Experience from './components/Experience';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Search from './components/Search';
 
-export default function App() {
+function App() {
   return (
-    <div className="App">
-      <Header />
+    <div className="App text-white overflow-hidden">
+        <Header/>
+        <Hero/>
+        <Experience/>
+        <Search/>
+        <Download/>
+        <Footer/>
     </div>
-  );
+  )
 }
+
+export default App;
